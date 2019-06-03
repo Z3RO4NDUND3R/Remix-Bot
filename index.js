98caabbdfee5aeac4e55d068e19e67a59e6396f3
Threqt <45086473+Threqt@users.noreply.github.com>
2019-06-03 01:21:05 -0500

    Added basic command to Ionix

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
+//Init vars
 const config = require("./botconfig")
 const Discord = require("discord.js");
 const bot = new Discord.Client({
   disableEverybody: true
 })
+const embedColor = '#000000'
 
+//Async functions
+
+//Startup events
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online!`);
 });
 
+//Command events
 bot.on("message", async message => {
   if (message.author.bot) return;
 
@@ -15,6 +21,13 @@ bot.on("message", async message => {
 
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const cmd = args.shift().toLowerCase();
+
+  if (cmd === `ping`) {
+    const pingEmbed = new Discord.RichEmbed()
+      .setColor(embedColor)
+      .setDescription(`:stopwatch: ${client.ping}`)
+    message.channel.send(pingEmbed)
+  }
 })
 
 bot.login(config.token);
