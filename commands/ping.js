34eb5f7afa37b4c3a9bbca26f255cac329c9c16f
Threqt <threqt@outlook.com>
2019-06-04 04:34:47 +0000

    Fixed ping

--- commands/ping.js
+++ commands/ping.js
@@ -1,8 +1,10 @@
 const Discord = require("discord.js");
 
-exports.run = (bot, message, args) => {
+exports.run = async (bot, message, args) => {
+  const msg = await message.channel.send("Testing ping...");
   const pingEmbed = new Discord.RichEmbed()
     .setColor(bot.embedColor)
-    .setDescription(`:stopwatch: ${Math.floor(bot.ping)}`)
+    .setDescription(`:stopwatch: ${Math.floor(bot.ping)}ms \n :hourglass_flowing_sand: : ${msg.createdTimestamp - message.createdTimestamp}ms`)
+  msg.delete()
   message.channel.send(pingEmbed)
 };
